b7f29eebd5df91dabae734c5424ece73ad7d78bd
Artur <artur@apocalypse.(none)>
2015-05-08 16:07:15 -0300

    Com SDL

--- Class/Nave.h
+++ Class/Nave.h
@@ -3,12 +3,16 @@
 class Nave : public Object{
 	private:
     	int health;
+      int angulo;
 	public:
   		Nave(int, int, int, int);
   		~Nave();
   		//Health
-  		void set_health(int);
-  		int get_health();
+      void set_health(int);
+      int get_health();
+      //Angulo
+      void set_angulo(int);
+      int get_angulo();
 };
 
 Nave::Nave(int w, int h, int x_parameter, int y_parameter) : Object(w, h, x_parameter, y_parameter) {
@@ -18,8 +22,16 @@ Nave::Nave(int w, int h, int x_parameter, int y_parameter) : Object(w, h, x_para
 
 //Health
 void Nave::set_health(int h){
-	health = h;
+  health = h;
 }
 int Nave::get_health(){
-	return health;
+  return health;
+}
+
+//Angulo
+void Nave::set_angulo(int h){
+  angulo = h;
+}
+int Nave::get_angulo(){
+  return angulo;
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "SDL/SDL_ttf.h"
+#include "SDL/SDL_mixer.h"
+
+
 #include "Class/Object.h"
 #include "Class/Eve.h"
 #include "Class/Nave.h"
@@ -9,8 +15,44 @@
 using namespace std;
 
 
-int main(int argc, char const *argv[])
-{
-	cout << "asdas" << endl;
+int main(int argc, char const *argv[]){
+	
+	srand(time(NULL));
+	
+
+	//INITIALIZING SDL
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_WM_SetCaption("DroneNudes", NULL);
+	TTF_Init();
+	Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 );
+	SDL_Surface *screen = NULL;
+	screen=SDL_SetVideoMode(800,640,16,SDL_SWSURFACE);
+
+	bool running = true;
+
+	SDL_Event event;
+
+	while(running){
+
+		while(SDL_PollEvent(&event)){
+			if( event.type==SDL_QUIT){
+				running = false;
+			}
+			if( event.type == SDL_MOUSEMOTION )	{
+				
+			}
+			// if(event.type == SDL_KEYDOWN){
+			// 	switch( event.key.keysym.sym )
+			// 	{
+			// 		case SDLK_UP: { player->moveShip(screen); break;}
+			// 		case SDLK_LEFT:{ player->increaseAngle(); break;}
+			// 		case SDLK_RIGHT:{player->decreaseAngle(); break;}
+			// 	}
+			// }
+		}
+
+
+	}
+
 	return 0;
 }
